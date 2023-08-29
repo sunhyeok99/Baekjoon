e6211aa3c9a40a2e2ab83d21ce8c6be60f9cdc80
slside <132821972+slside@users.noreply.github.com>
2023-08-29 16:18:24 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 12716 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {		
+		Scanner sc=  new Scanner(System.in);
+
+
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+}
