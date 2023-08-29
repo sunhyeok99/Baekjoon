dc462d304873721b9567a52485057ffa49d82b0a
slside <132821972+slside@users.noreply.github.com>
2023-08-29 15:40:06 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {		
+		Scanner sc=  new Scanner(System.in);
+//		int N = sc.nextInt();
+		int[] arr = new int[9];
+		for(int i = 0;i<9;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max=0;
+		int maxi =0;
+		for(int i=0;i<9;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+				maxi= i+1;
+			}
+		}
+		System.out.println(max);
+System.out.println(maxi);
+	}
+}
