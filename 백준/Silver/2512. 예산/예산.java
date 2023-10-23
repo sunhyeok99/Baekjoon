5ee65c14a083a728e2be4ce5b057ab429ed79df1
slside <132821972+slside@users.noreply.github.com>
2023-10-23 10:45:23 +0900

    [Silver II] Title: 예산, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,59 +1,54 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static long[] arr;
     public static void main(String[] args) throws IOException {
-    	Scanner sc = new Scanner(System.in);
-    	N = sc.nextInt();
-    	arr = new long[N];
-    	for(int i =0;i<N;i++) {
-    		arr[i] = sc.nextInt();
-    	}
-    	long max = sc.nextLong();
-    	// 약간 이거 
-    	// 일단 정렬해가지고 
-    	// 허용되는 숫자랑 넘는숫자 두개가지고
-    	// 이분탐색 하는 느낌
-    	
-    	// 일단 정렬
-    	Arrays.sort(arr);
-    	// 정렬해서 일단 더하다가 안넘는 범위랑 
-    	// 넘어가는 범위를 구해보자 
-    	long lo = 0;
-    	long hi = arr[N-1];
-    	// 2분탐색으로 index 사이를 구해보자
-    	long middle = 0;
-    	while(true) {
-    		middle = (lo + hi)/2;
-    		
-    		long sum = 0;
-    		for(int i =0;i<N;i++) {
-    			long tmp = Math.min(arr[i], middle);
-    			sum += tmp;
-    		} 
-//    		System.out.println(sum+" "+middle);
-    		if(sum == max) {
-    			break;
-    		}
-    		else if(sum>max) {
-    			hi = middle-1;
-    		}
-    		else {
-    			lo = middle +1;
-    		}
-    		
-    		if(lo>hi) {
-    			middle = hi;
-    			break;
-    		}
-    		
-    	}
-    	
-    	System.out.println(middle);
-    	
-    	
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+//        String st = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum=0;
+//        int max=0;
+        for(int i=0;i<N;i++) {
+//            arr[i] = Integer.parseInt(st.split(" ")[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
+//            if(arr[i]>max) max = arr[i];
+        }
+        int tBud = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+            int lo = 0;
+            int hi = arr[N-1];
+            int mid =0; int ans=0;
+            while(true) {
+                mid = (lo + hi)/2;
+                sum=0;
+                for(int i=0;i<N;i++) {
+//                    if(arr[i]<mid) {
+//                        sum += arr[i];
+//                    }else {
+//                        sum += mid;
+//                    }
+                    sum += Math.min(arr[i], mid);
+                }
+                
+                if(sum>tBud) hi = mid-1;
+                else if(sum == tBud){
+                	ans = mid;
+                	break;
+                }
+//                if(sum<tBud) {
+                else {
+                    lo = mid+1;
+                }
+                if(lo>hi) {
+                	ans = hi;
+                	break;
+                }
+            }
+            System.out.println(ans);
+    }//main
 }
