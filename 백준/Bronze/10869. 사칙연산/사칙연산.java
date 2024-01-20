b9f64a71aebebf2894a39134a7e28dff9ac67fcd
Kim SunHyeok <132821972+slside@users.noreply.github.com>
2024-01-20 23:15:01 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 13000 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(N+M);
+		System.out.println(N-M);
+		System.out.println(N*M);
+		System.out.println(N/M);
+		System.out.println(N%M);
+		
+	}		
+}
