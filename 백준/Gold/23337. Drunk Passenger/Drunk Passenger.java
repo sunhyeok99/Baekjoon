4404e2c38593a55e9c208413539c635a0015784a
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-02-27 13:30:46 +0900

    [Gold V] Title: Drunk Passenger, Time: 112 ms, Memory: 12976 KB -BaekjoonHub

--- 백준/Gold/23337. Drunk Passenger/Drunk Passenger.java
+++ 백준/Gold/23337. Drunk Passenger/Drunk Passenger.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 
 public class Main {
 	static int N;
-	static int[][] arr;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		arr = new int[N][N];
 	// dp 같긴한데
 		// 2 -> 2/2
 		// 3 -> 3/4
 		// 4 ->	4/6
+		// 위에함은 +1씩
+		// 밑에항은 +2씩
 		double a = N;
 		double b = 2*N -2;
 		System.out.println(a/b);
 		
+		// 이거 왜 정답임? ㅋㅋ 
 	}
 }
