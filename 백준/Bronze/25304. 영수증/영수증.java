2d5a222c25f382c5cdf491ca853a8651d06aed84
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-03-20 20:52:21 +0900

    [Bronze IV] Title: 영수증, Time: 124 ms, Memory: 13088 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+        int total = 0;
+        for(int i = 0; i < N; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            total += a * b;
+        }
+        if(X == total) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
