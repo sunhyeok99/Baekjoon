6fb4936a838334cf4775afc5240c8cc87ffbd84f
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-08-04 14:41:06 +0900

    [level 2] Title: 타겟 넘버, Time: 5.29 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int[] numbers;
+    static int target;
+    static int n;
+    static int ans;
+    public int solution(int[] numbers, int target) {
+        // 2가지 방법 가능
+        this.numbers = numbers;
+        this.target = target;
+        n = numbers.length;
+        solve(0,0);
+        
+        return ans;
+    }
+    public static void solve(int index , int sum){
+        if(index == n){
+            if(sum == target){
+                ans++;
+            }
+            return;
+        }
+        
+        solve(index+1, sum+numbers[index]);
+        solve(index+1, sum - numbers[index]);
+        
+    }
+    
+    
+}
