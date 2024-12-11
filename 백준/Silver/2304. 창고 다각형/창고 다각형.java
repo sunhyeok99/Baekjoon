18fcd765b12981048c79fac77b5bb8a9a89e00e3
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-12-12 03:29:32 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,66 +1,51 @@
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
+	static int N;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();	
-		//		ArrayList<Integer> arr =new ArrayList<>();;
-		int[][] tmp = new int[N][2];
-		int maxi =0;
-		int max=0;
-		int maxindex = 0;
-		for(int i =0;i<N;i++) {
-			tmp[i][0] = sc.nextInt();
-			if(tmp[i][0]>maxi) {
-				maxi = tmp[i][0];
-			} // maxi+1만큼 배열만들기 위해서  구함
-			tmp[i][1] = sc.nextInt();	
-			if(tmp[i][1] >max) {
-				max= tmp[i][1]; // 전체배열에서의 최고높이
-				maxindex = tmp[i][0]; // 최고높이일때의 index
-			}
+		N = sc.nextInt();
+		// 가장 큰 기둥을 기준으로 
+		// 값을 저장해놓고 왼쪽과 오른쪽 맨끝에서 서로 가장큰 기둥이 나올때까지 max값 갱신
+		// 가운데와 가운데 사이는 따로 플러스
+		int[] arr = new int[1001];
+		int maxI = 0;
+		int max = 0;
+		for(int i = 0 ; i < N;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			maxI = Math.max(maxI, a);
+			max = Math.max(max, b);
+			arr[a] = b;
 		}
-
-		int[] arr = new int[maxi+1];
-		for(int i =0;i<N;i++) {
-			arr[tmp[i][0]] = tmp[i][1];
-		}
-		int sum = 0; // 정답 넓이
-		int num=0; // 진행중인 과정에서의 최대 값 
-		int i =0; // 시작 index = 0부터시작			
-		while(true) {
-			if(num<arr[i]) {
-				num = arr[i];
-			}
-			sum+=num;			
-			i++;
-			// l은 1이상이기 때문에 0일때 고려하지 않아도 괜찮 
-			// 0이라고려하면 최고값이 0에 있을때 고려해야함
-			if(i==maxindex) {
+		// max를 기준으로 양끝에서 시작
+		int sum = 0 ;
+		int tmp = 0;
+		int st = 0;
+		int en = 0;
+		for(int i= 0 ;i<=maxI;i++) {
+			tmp = Math.max(tmp, arr[i]);
+			if(tmp == max) {
+				st = i;
 				break;
 			}
+			sum += tmp;
 		}
-		num = 0; // num은 0으로 초기화
-		i = maxi; // 끝에서부터
-		while(true) {
-			if(num<arr[i]) {
-				num = arr[i];
-			}
-			sum+= num;
-			i--;
-			if(i==maxindex-1) {
+		tmp = 0;
+		for(int i = maxI; i>=0;i--) {
+			tmp = Math.max(tmp, arr[i]);
+			if(tmp == max) {
+				en = i;
 				break;
 			}
-			
+			sum += tmp;
 		}
+		// 이제 최대 높이기둥 사이만 더하면됨
+		sum += arr[st] * (en - st +1);
+		
 		System.out.println(sum);
-
-
 	}
-
-
-
+	
+	
 }
