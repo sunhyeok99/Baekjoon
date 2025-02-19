c8a07131b02fe3092c76ac70f2f92391966758cd
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2025-02-19 13:51:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 92 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.next().charAt(0);
+        System.out.println(N);
+
+    }
+
+}
