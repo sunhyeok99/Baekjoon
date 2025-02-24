03fb9906d9cd016fa66403cb0ff3332d2fd4d13a
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2025-02-24 11:22:26 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 3468 ms, Memory: 436628 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static ArrayList<Integer>[] ar;
+    static int[][] dp;
+    static int ans;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 일단 하나인것들은 찾아서 연결된 것과 true처리
+        // 하나가 끝나면 이제 나머지 것들은 +1해줌
+        N = sc.nextInt();
+        ans = 0;
+        ar = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++){
+            ar[i] = new ArrayList<>();
+        }
+        for(int i = 1; i <= N-1; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            ar[a].add(b);
+            ar[b].add(a);
+        }
+
+        // 얼리어답터 선정 or 비선정
+        dp = new int[N+1][2];
+        solve(0,1);
+        System.out.println(Math.min(dp[1][0], dp[1][1]));
+
+    }
+    public static void solve(int parent, int x){
+        // 여기가 얼리어답터 일경우에는 다음 브랜치가 상관없고,
+        // 아니면 다음 브랜치는 무조건 얼리어답터 이어야한다.
+        dp[x][0] =0;
+        dp[x][1] =1;
+
+        for(int i = 0; i < ar[x].size(); i++){
+            int nx = ar[x].get(i);
+            if(nx == parent){
+                continue;
+            }
+            solve(x, nx);
+            // 다음거는 무조건 얼리어답터
+            dp[x][0] += dp[nx][1];
+            // 상관없음
+            dp[x][1] += Math.min(dp[nx][0] , dp[nx][1]);
+        }
+
+
+    }
+
+}
