979913781be3661257dc7c6eaad07f033b2b44c2
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2025-03-20 16:36:51 +0900

    [Gold IV] Title: 사냥꾼, Time: 984 ms, Memory: 232396 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.java
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -1,59 +1,55 @@
 import java.util.*;
 
 public class Main {
-    static class node implements Comparable<node>{
-        int x;
-        int y;
-        node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        public int compareTo(node o){
-            return this.x - o.x;
-        }
-    }
     static int N,M;
     static int[] arr;
-    static PriorityQueue<node> q;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        arr = new int[N+1];
+        arr = new int[N];
         int L = sc.nextInt();
 
         for(int i = 0;i < N;i++) {
             arr[i] = sc.nextInt();
         }
-        arr[N] = arr[N-1];
-        q = new PriorityQueue<>();
+        int[][] animal = new int[M][2];
         for(int i = 0;i < M;i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            q.add(new node(a,b));
+            animal[i][0] = a;
+            animal[i][1] = b;
         }
         Arrays.sort(arr);
         // 하나씩 꺼내면서 비교
         int ans = 0;
-       for(int i = 0;i <= N;i++) {
-           int pos = arr[i];
-            while(q.size()!=0){
-                node n = q.peek();
-                if(Math.abs(n.x - pos) + n.y <= L){
-                    // 사정거리 안
-                    q.poll();
+
+        for(int i = 0 ; i < M;i++){
+            // 동물하나를 잡고 이분탐색을 통해 사정거리 안에 들어가는 지 판단
+            // 들어가면 다음 동물 / 안들어가면 될때까지
+            int a = animal[i][0];
+            int b = animal[i][1];
+            int left = 0;
+            int right = N-1;
+            while(left <= right){
+                int mid = (left + right) / 2;
+                if(Math.abs(arr[mid] - a) + b <= L){
                     ans ++;
-                }
-                else{
-                    // 만약 x 좌표가 가까운데 거리가 먼경우
-                    if(i != N && (Math.abs(arr[i+1] - n.x) >= Math.abs(arr[i] - n.x))){
-                        q.poll();
-                        continue;
-                    }
                     break;
                 }
+                // 사정거리 안에 들어가면 ans++
+
+                // x좌표가 arr기준 어디위치하는지에 따라 이동
+                // x좌표가 더 적다면 arr을 낮추고 / 더 크다면 left를 올려서 기준올림
+                if(a < arr[mid]){
+                    right = mid -1;
+                }
+                else{
+                    left = mid +1;
+                }
             }
-       }
+        }
+
 
         System.out.println(ans);
 
