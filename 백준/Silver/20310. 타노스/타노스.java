48062ccb7c1ea833fef4e58bb57e92a585206fdc
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2025-08-11 11:30:47 +0900

    [Silver III] Title: 타노스, Time: 96 ms, Memory: 13544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -23,19 +23,24 @@ public class Main {
 		}
 		a /=2;
 		b /=2;
-		String answer = "";
-		for(int i = 0 ;i< a;i++){
-			answer += "0";
-		}
-		for(int i = 0 ; i < b;i++){
-			answer += "1";
-		}
-		System.out.println(answer);
-
 		// 우선 0과 1의 개수를 count 하고
 		// 0의 개수 + 1의 개수 하면 될듯?
 
-
+		// --> 시간 절약하려면 앞에서부터 1 제거, 뒤에서부터 0제거하면된다.
+		for(int i = 0; i < s.length() && b > 0; i++) {
+			if(s.charAt(i) == '1'){
+				s = s.substring(0, i) + s.substring(i+1);
+				b --;
+				i --;
+			}
+		}
+		for(int i = s.length()-1; i >= 0 && a > 0; i--) {
+			if(s.charAt(i) == '0'){
+				s = s.substring(0, i) + s.substring(i+1);
+				a --;
+			}
+		}
+		System.out.println(s);
 
 
 
